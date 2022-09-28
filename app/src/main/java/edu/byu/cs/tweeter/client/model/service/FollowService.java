27df499215db053ae1de8b3b0e5dd41304898054
PJ Rape <ptrj96@gmail.com>
2022-09-27 21:04:01 -0600

    refactor followers

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.backgroundTask.GetFollowersTask;
 import edu.byu.cs.tweeter.client.backgroundTask.GetFollowingTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
@@ -45,11 +46,44 @@ public class FollowService {
             } else if (msg.getData().containsKey(GetFollowingTask.MESSAGE_KEY)) {
                 String message = msg.getData().getString(GetFollowingTask.MESSAGE_KEY);
                 observer.handleGetFollowingFailure(message);
-//                Toast.makeText(getContext(), "Failed to get following: " + message, Toast.LENGTH_LONG).show();
             } else if (msg.getData().containsKey(GetFollowingTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
                 observer.handleGetFollowingThrewException(ex);
-//                Toast.makeText(getContext(), "Failed to get following because of exception: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public  interface FollowerObserver {
+        void handleGetFollowerSuccess(List<User> followers, boolean morePages);
+        void handleGetFollowerFailure(String message);
+        void handleGetFollowerThrewException(Exception ex);
+    }
+    public void getFollower(AuthToken token, User user, int limit, User lastFollower, FollowerObserver observer) {
+        GetFollowersTask getFollowersTask = new GetFollowersTask(token, user, limit, lastFollower, new GetFollowersHandler(observer));
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(getFollowersTask);
+    }
+    /**
+     * Message handler (i.e., observer) for GetFollowersTask.
+     */
+    private class GetFollowersHandler extends Handler {
+        private FollowerObserver observer;
+        public GetFollowersHandler(FollowerObserver observer) {
+            this.observer = observer;
+        }
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetFollowersTask.SUCCESS_KEY);
+            if (success) {
+                List<User> followers = (List<User>) msg.getData().getSerializable(GetFollowersTask.FOLLOWERS_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFollowersTask.MORE_PAGES_KEY);
+                observer.handleGetFollowerSuccess(followers, hasMorePages);
+            } else if (msg.getData().containsKey(GetFollowersTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFollowersTask.MESSAGE_KEY);
+                observer.handleGetFollowerFailure(message);
+            } else if (msg.getData().containsKey(GetFollowersTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFollowersTask.EXCEPTION_KEY);
+                observer.handleGetFollowerThrewException(ex);
             }
         }
     }
